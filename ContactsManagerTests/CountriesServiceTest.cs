202d074d4759b40fd4661e753324af0466b3767e
Imon Islam <imon0077@yahoo.com>
2023-08-25 11:24:26 +0600

    Add project files.

---
+++ ContactsManagerTests/CountriesServiceTest.cs
@@ -0,0 +1,81 @@
+﻿using ServiceContracts;
+using ServiceContracts.DTO;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsManagerTests
+{
+    public class CountriesServiceTest
+    {
+        private readonly ICountriesService _countriesService;
+
+        public CountriesServiceTest()
+        {
+            _countriesService = new CountriesService();
+        }
+
+        //When CountryAddRequest is null, it should throw ArgumentNullExeption
+        [Fact]
+        public void AddCountry_Nullcountry()
+        {
+            //Arrange
+            CountryAddRequest? request = null;
+
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => {
+                //Act
+                _countriesService.AddCountry(request);
+            });
+        }
+
+        //When the Country is null, it should throw ArgumentException
+        [Fact]
+        public void AddCountry_CountryNameIsNull()
+        {
+            //Arrange
+            CountryAddRequest? request = new CountryAddRequest() { CountryName = null};
+
+            //Assert
+            Assert.Throws<ArgumentException>(() => {
+                //Act
+                _countriesService.AddCountry(request);
+            });
+        }
+
+
+        //When the CountryName is duplicate, it should throw ArgumentException
+        [Fact]
+        public void AddCountry_DuplicateCountryName()
+        {
+            //Arrange
+            CountryAddRequest? request1 = new CountryAddRequest() { CountryName = "USA" };
+            CountryAddRequest? request2 = new CountryAddRequest() { CountryName = "USA" };
+
+            //Assert
+            Assert.Throws<ArgumentException>(() => {
+                //Act
+                _countriesService.AddCountry(request1);
+                _countriesService.AddCountry(request2);
+            });
+        }
+
+        //When you supply proper  country name, it should insert (add) the country to the existing list.
+        [Fact]
+        public void AddCountry_ProperCountryDetails()
+        {
+            //Arrange
+            CountryAddRequest? request = new CountryAddRequest() { CountryName = "Japan" };
+
+            //Act
+            CountryResponse response = _countriesService.AddCountry(request);
+
+            //Assert
+            Assert.True(response.CountryID != Guid.Empty);
+           
+        }
+    }
+}
---
+++ ServiceContracts/ICountriesService.cs
@@ -0,0 +1,17 @@
+﻿using ServiceContracts.DTO;
+
+namespace ServiceContracts
+{
+    /// <summary>
+    /// Represents business logic for manipulating country entity
+    /// </summary>
+    public interface ICountriesService
+    {
+        /// <summary>
+        /// Adds a country object to a list of countries
+        /// </summary>
+        /// <param name="countryAddRequest">Country object to add</param>
+        /// <returns>Return the country object after adding it(incliding newly generated country id)</returns>
+        CountryResponse AddCountry(CountryAddRequest? countryAddRequest);
+    }
+}
