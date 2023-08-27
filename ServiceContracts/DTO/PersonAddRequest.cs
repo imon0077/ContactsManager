c3ee65aeed6d3a362850e3c03899be74b47a98ea
Imon Islam <imon0077@yahoo.com>
2023-08-27 21:11:38 +0600

    Generated Person model, PersonAddRequest DTO & GenderOptions Enums

---
+++ ServiceContracts/DTO/PersonAddRequest.cs
@@ -0,0 +1,18 @@
+﻿using ServiceContracts.Enums;
+
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// Acts as a DTO for inserting a new person
+    /// </summary>
+    public class PersonAddRequest
+    {
+        public string? PersonName { get; set; }
+        public string? Email { get; set; }
+        public GenderOptions? Gender { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public Guid? CountryID { get; set; }
+        public string? Address { get; set; }
+        public bool ReceiveNewsLetters { get; set; }
+    }
+}
