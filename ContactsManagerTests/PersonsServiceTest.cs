dc5bad01279872a5eb407345be1c97dd8bf303ce
Imon Islam <imon0077@yahoo.com>
2023-08-28 20:15:43 +0600

    xUnit - Test case added for AddPerson

---
+++ ContactsManagerTests/PersonsServiceTest.cs
@@ -0,0 +1,72 @@
+﻿using ServiceContracts;
+using ServiceContracts.DTO;
+using Services;
+using ServiceContracts.Enums;
+
+namespace ContactsManagerTests
+{
+    public class PersonsServiceTest
+    {
+        private readonly IPersonsService _personsService;
+
+        public PersonsServiceTest()
+        {
+            _personsService = new PersonsService();
+        }
+
+        #region AddPerson
+        [Fact]
+        public void AddPerson_NullPerson()
+        {
+            //Arrange
+            PersonAddRequest? request = null;
+
+            //Act
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                _personsService.AddPerson(request);
+            });
+        }
+
+        [Fact]
+        public void AddPerson_PersonNameIsNull()
+        {
+            //Arrange
+            PersonAddRequest request = new PersonAddRequest { PersonName = null };
+
+            //Act
+            Assert.Throws<ArgumentException>(() =>
+            {
+                _personsService.AddPerson(request);
+            });
+        }
+
+        [Fact]
+        public void AddPerson_ProperPersonDetails()
+        {
+            //Arrange
+            PersonAddRequest request = new PersonAddRequest
+            {
+                PersonName = "Person name...",
+                Email = "person@email.com",
+                Address = "Sample Address ",
+                CountryID = Guid.NewGuid(),
+                Gender = GenderOptions.Male,
+                DateOfBirth = DateTime.Parse("2000-01-01"),
+                ReceiveNewsLetters = true
+            };
+
+            //Act
+            PersonResponse personResponse = _personsService.AddPerson(request);
+
+            List<PersonResponse> person_list = _personsService.GetAllPersons();
+
+            //Assert
+            Assert.True(personResponse.PersonID != Guid.Empty);
+
+            Assert.Contains(personResponse, person_list);
+        }
+
+        #endregion
+    }
+}
---
+++ ServiceContracts/IPersonsService.cs
@@ -0,0 +1,24 @@
+﻿
+using ServiceContracts.DTO;
+
+namespace ServiceContracts
+{
+    /// <summary>
+    /// Represents Business Logic for manupulating Person entity
+    /// </summary>
+    public interface IPersonsService
+    {
+        /// <summary>
+        /// Add a new person into the list of Persons
+        /// </summary>
+        /// <param name="personAddRequest">Person to add</param>
+        /// <returns>Return same person details along with newly generated PersonID</returns>
+        PersonResponse AddPerson(PersonAddRequest? personAddRequest);
+
+        /// <summary>
+        /// Return all persons
+        /// </summary>
+        /// <returns>Return a list of objects of PersonResponse type</returns>
+        List<PersonResponse> GetAllPersons();
+    }
+}
