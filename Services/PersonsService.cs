2126e2ee1534a8f91c92bddb87a88e026bd1b064
Imon Islam <imon0077@yahoo.com>
2023-08-28 21:52:00 +0600

    AddPerson implementation in PersonService

--- Services/PersonsService.cs
+++ Services/PersonsService.cs
@@ -1,13 +1,48 @@
-﻿using ServiceContracts;
+﻿using Entities;
+using ServiceContracts;
 using ServiceContracts.DTO;
 
 namespace Services
 {
     public class PersonsService : IPersonsService
     {
+        private readonly List<Person> _persons;
+        private readonly ICountriesService _countriesService;
+
+        public PersonsService()
+        {
+            _persons = new List<Person>();
+            _countriesService = new CountriesService();
+        }
+
+        private PersonResponse ConvertPersonToPersonResponse(Person person)
+        {
+            PersonResponse personResponse = person.ToPersonResponse();
+            personResponse.Country = _countriesService.GetCountryByCountryID(personResponse.CountryID)?.CountryName;
+            return personResponse;
+        }
+
         public PersonResponse AddPerson(PersonAddRequest? personAddRequest)
         {
-            throw new NotImplementedException();
+            //check if personAddRequest is null
+            if (personAddRequest == null)
+                throw new ArgumentNullException(nameof(personAddRequest));
+
+            //check if person name is null
+            if (string.IsNullOrEmpty(personAddRequest.PersonName))
+                throw new ArgumentException("Person name cant be blank");
+
+            //convert PersonAddRequest into Person type
+            Person person = personAddRequest.ToPerson();
+
+            //Generate PersonID
+            person.PersonID = Guid.NewGuid();
+
+            //Add person to person list
+            _persons.Add(person);
+
+            //Convert the person object into PersonRespinse type
+            return ConvertPersonToPersonResponse(person);
         }
 
         public List<PersonResponse> GetAllPersons()
